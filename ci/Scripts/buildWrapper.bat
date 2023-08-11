1113481e955417fc249442a65bea5b622f599f5b
Constantin Belyaev <cbelyaev@gmail.com>
2023-08-11 15:13:54 +0500

    release 1.1.1.0

--- ci/Scripts/buildWrapper.bat
+++ ci/Scripts/buildWrapper.bat
@@ -5,10 +5,6 @@ call "%~dp0\getvars.bat"
 pushd .
 cd %freeRdpDir%\Primo.FreeRdpWrapper
 
-rem restore dependencies
-echo ">>>>>>>>>>>>>> restore wrapper dependencies"
-%msbuild% "Primo.FreeRdpWrapper.sln" /t:restore
-
 rem build wrapper libs
 echo ">>>>>>>>>>>>>> building wrapper configuration:Debug"
 %msbuild% "Primo.FreeRdpWrapper.sln" /p:Configuration=Debug /p:Platform=x64
